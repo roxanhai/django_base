212844bbca8f4434bee3090fbf3ab7ee4df1d493
Bui Hai An <anbh2@ghtk.co>
2023-07-01 15:41:39 +0700

    git cache cleared

--- apps/models/base_models.py
+++ apps/models/base_models.py
@@ -6,9 +6,6 @@ class Reporter(models.Model):
 
     def __str__(self):
         return self.name
-
-class Reporter2(models.Model):
-    name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.name
+    
+    class Meta:
+        db_table='reporter'
